8cbda9abd023b878ea1820d5556d9c504bd32380
Kuhtal <leonidpetrovsky@gmail.com>
2023-04-04 19:11:39 +0300

    adding db function for adding user

--- stuff.py
+++ stuff.py
@@ -1,6 +1,7 @@
 import requests as r
 import geopy
 import os
+import sqlite3
 from datetime import datetime
 
 
@@ -41,3 +42,18 @@ def get_forecast(lat, lon):
     }
     resp = r.get(url, params=params).json()
     return resp
+def connect_db():
+    con = sqlite3.connect('identifier.sqlite')
+    return con
+def add_user(f_name,l_name,tg_id,phone,connectition):
+    sql = f'''INSERT INTO users (first_name, last_name, tg_id, phone_number)
+    VALUES ({f_name},{l_name},{tg_id},{phone})'''
+    try:
+
+        curs = connectition.cursor()
+        curs.execute(sql)
+        return 0
+    except:
+        return None
+
+
