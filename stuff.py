b0ea5fb14c10223a408211241ece1bc53573cd2b
100Ballov <lebedevnikita754@gmail.com>
2023-04-11 18:24:45 +0300

    bot_functions.py

--- stuff.py
+++ stuff.py
@@ -1,5 +1,4 @@
 import requests as r
-import geopy
 import os
 import sqlite3
 from datetime import datetime
@@ -24,11 +23,7 @@ def send_image():
     return img_url
 
 
-def get_city(lat, lon):
-    locator = geopy.geocoders.Nominatim(user_agent='geoapiExercises')
-    location = locator.reverse(str(lat) + ',' + str(lon))
-    address = location.raw['address']
-    return address
+
 
 
 def get_forecast(lat, lon):
@@ -41,6 +36,20 @@ def get_forecast(lat, lon):
         'lang': 'ru',
     }
     resp = r.get(url, params=params).json()
+    text =  '<strong>'
+    res = ''
+    for date in resp['List']:
+        day = datetime.datetime.fromtimestamp(date['dt'])
+        date_res = date.strptime( '%Y-%m-%d')
+        temp = date['main']['temp']
+        weather = date['weather'][0]['description']
+
+        if date.hour == 15:
+            daytime = 'днем'
+            res += text.format(date_res,daytime,temp,weather)
+         elif  date.hour == 21:
+                daytime = 'вечером'
+                res += text.format(date_res, daytime, temp, weather)
     return resp
 def connect_db():
     con = sqlite3.connect('identifier.sqlite')
